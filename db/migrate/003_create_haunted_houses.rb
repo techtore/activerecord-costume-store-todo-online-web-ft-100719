4c598cac622203463943e100e6c918346c7b69aa
Victoria Hale <vic.hale74@gmail.com>
2019-11-10 22:07:36 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     t.boolean :family_friendly
     t.datetime :opening_date
     t.datetime :closing_date
-    t.string :description
-   
+    t.text :description
+   end
  end
 end# Create your haunted_houses migration here
