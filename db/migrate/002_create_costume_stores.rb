351ec06c60a7c8f70f7d234ff6a4a193d3b624c9
Victoria Hale <vic.hale74@gmail.com>
2019-11-10 22:01:26 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,10 +3,10 @@ class CostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamps
+      t.
   end
   end
 end    
