81e99f0a91dbb778c36fdef53f5a14e029023236
Victoria Hale <vic.hale74@gmail.com>
2019-11-10 22:01:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,6 +7,7 @@ class CostumeStores < ActiveRecord::Migration[4.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
+      t.datetime :closing_time
   end
   end
 end    
